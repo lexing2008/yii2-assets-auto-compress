30a1a7ac0c79f9a7716c1bd3d6f6d3030a74414e
lexing2008 <lexing2008@yandex.ru>
2021-11-08 17:34:08 +0300

    add functionality

--- src/AssetsAutoCompressComponent.php
+++ src/AssetsAutoCompressComponent.php
@@ -32,6 +32,17 @@ use yii\web\View;
 class AssetsAutoCompressComponent extends Component implements BootstrapInterface
 {
     /**
+     * Папка common
+     */
+    const COMMON_FOLDER = '/common/';
+
+    /**
+     * Папка backend 
+     * @var string
+     */
+    public $backendUrlFolder = '/admin/';
+
+    /**
      * Enable or disable the component
      * @var bool
      */
@@ -198,6 +209,37 @@ class AssetsAutoCompressComponent extends Component implements BootstrapInterfac
     }
 
     /**
+     * @return bool|string
+     */
+    public function getCommon()
+    {
+        return \Yii::getAlias('@common/');
+    }
+
+    /**
+     * @return bool|string
+     */
+    public function getBackend()
+    {
+        return \Yii::getAlias('@backend/web/');
+    }
+
+    /**
+     * Возвращает полное имя файла со всеми директориями
+     * @param string $filename имя файла
+     * @return string полное имя файла со всеми директориями
+     */
+    public function getFullFileName(string $filename){
+        if(strpos($filename, self::COMMON_FOLDER) === 0) {
+            return $this->getCommon() . str_replace(self::COMMON_FOLDER, '', $filename);
+        } elseif(strpos($filename, $this->backendUrlFolder) === 0){
+            return $this->getBackend() . str_replace($this->backendUrlFolder, '', $filename);
+        } else {
+            return $this->getWebroot() . $filename;
+        }
+    }
+
+    /**
      * @param $path
      * @return $this
      */
@@ -214,6 +256,8 @@ class AssetsAutoCompressComponent extends Component implements BootstrapInterfac
     {
         if ($app instanceof \yii\web\Application) {
             $app->view->on(View::EVENT_END_PAGE, function (Event $e) use ($app) {
+
+
                 /**
                  * @var $view View
                  */
@@ -257,7 +301,7 @@ class AssetsAutoCompressComponent extends Component implements BootstrapInterfac
     {
         //Компиляция файлов js в один.
         //echo "<pre><code>" . print_r($view->jsFiles, true);die;
-        if ($view->jsFiles && $this->jsFileCompile) {
+        if ($view-->jsFiles && $this->jsFileCompile) {
             \Yii::beginProfile('Compress js files');
             foreach ($view->jsFiles as $pos => $files) {
                 if ($files) {
@@ -285,7 +329,7 @@ class AssetsAutoCompressComponent extends Component implements BootstrapInterfac
 
 
         //Compiling css files
-        if ($view->cssFiles && $this->cssFileCompile) {
+        if ($view->cssFiles  && $this->cssCompress && $this->cssFileCompile) {
             \Yii::beginProfile('Compress css files');
             if ($this->cssFileCompileByGroups) {
                 $view->cssFiles = $this->_processAndGroupCssFiles($view->cssFiles);
@@ -392,15 +436,36 @@ JS
     }
 
     /**
+     * Возвращает массив timestamps указанных файлов
+     * @param array $files массив имен файлов
+     * @return array массив timestamps файлов
+     */
+    public function getTimestampsFiles(array $files){
+        $timestamps = [];
+        foreach ($files as $file){
+            if (Url::isRelative($file)) {
+                $fileName = preg_replace('/\?.*/ui', '', $file);
+                $fileName = $this->getFullFileName($fileName);
+                if(file_exists($fileName)){
+                    $timestamps[] = filemtime($fileName);
+                } else {
+                    throw new \Exception("File {$file}  {$fileName} not found");
+                }
+            }
+        }
+        return $timestamps;
+    }
+
+    /**
      * @param array $files
      * @return array
      */
     protected function _processingJsFiles($files = [])
     {
-        $fileName = md5(implode(array_keys($files)).$this->getSettingsHash()).'.js';
+        $filesNames = array_keys($files);
+        $fileName = md5(implode($filesNames) . implode($this->getTimestampsFiles($filesNames)) .$this->getSettingsHash()).'.js';
         $publicUrl = \Yii::$app->assetManager->baseUrl.'/js-compress/'.$fileName;
         //$publicUrl  = \Yii::getAlias('@web/assets/js-compress/' . $fileName);
-
         $rootDir = \Yii::$app->assetManager->basePath.'/js-compress';
         //$rootDir    = \Yii::getAlias('@webroot/assets/js-compress');
         $rootUrl = $rootDir.'/'.$fileName;
@@ -416,8 +481,7 @@ JS
                 }
             }
 
-
-            $publicUrl = $publicUrl."?v=".filemtime($rootUrl);
+            $publicUrl = $publicUrl.'?v='.filemtime($rootUrl);
             $resultFiles[$publicUrl] = Html::jsFile($publicUrl, $this->jsOptions);
             return $resultFiles;
         }
@@ -431,8 +495,8 @@ JS
                     if ($pos = strpos($fileCode, "?")) {
                         $fileCode = substr($fileCode, 0, $pos);
                     }
-
-                    $fileCode = $this->webroot.$fileCode;
+                    //$fileCode = $this->webroot.$fileCode;
+                    $fileCode = $this->getFullFileName($fileCode);
                     $contentFile = $this->readLocalFile($fileCode);
 
                     /**\Yii::info("file: " . \Yii::getAlias(\Yii::$app->assetManager->basePath . $fileCode), self::class);*/
@@ -462,6 +526,7 @@ JS
                 }
             }
 
+
             if ($this->jsFileCompress) {
                 $content = \JShrink\Minifier::minify($content, ['flaggedComments' => $this->jsFileCompressFlaggedComments]);
             }
@@ -476,7 +541,6 @@ JS
             fwrite($file, $content);
             fclose($file);
         }
-
 
         if (file_exists($rootUrl)) {
             $publicUrl = $publicUrl."?v=".filemtime($rootUrl);
@@ -586,7 +650,8 @@ JS
      */
     protected function _processingCssFiles($files = [])
     {
-        $fileName = md5(implode(array_keys($files)).$this->getSettingsHash()).'.css';
+        $filesNames = array_keys($files);
+        $fileName = md5(implode($filesNames) . implode($this->getTimestampsFiles($filesNames)) .$this->getSettingsHash()).'.css';
         $publicUrl = \Yii::$app->assetManager->baseUrl.'/css-compress/'.$fileName;
         //$publicUrl  = \Yii::getAlias('@web/assets/css-compress/' . $fileName);
 
@@ -621,7 +686,8 @@ JS
                         $fileCodeLocal = substr($fileCodeLocal, 0, $pos);
                     }
 
-                    $fileCodeLocal = $this->webroot.$fileCodeLocal;
+                    //$fileCodeLocal = $this->webroot.$fileCodeLocal;
+                    $fileCodeLocal = $this->getFullFileName($fileCodeLocal);
                     $contentTmp = trim($this->readLocalFile($fileCodeLocal));
 
                     //$contentTmp         = trim($this->fileGetContents( Url::to(\Yii::getAlias($fileCode), true) ));
@@ -629,7 +695,6 @@ JS
                     $fileCodeTmp = explode("/", $fileCode);
                     unset($fileCodeTmp[count($fileCodeTmp) - 1]);
                     $prependRelativePath = implode("/", $fileCodeTmp)."/";
-
                     $contentTmp = \Minify_CSS::minify($contentTmp, [
                         "prependRelativePath" => $prependRelativePath,
 
